fdb4c0bf7e5cc5b9514f8211b5ccb7d17e3f75a1
GuiPimenta--Dev <guialvespimenta27@gmail.com>
2021-05-19 13:30:48 -0300

    image logo

--- components/VerticalNavBar/VerticalNavBar.js
+++ components/VerticalNavBar/VerticalNavBar.js
@@ -1,54 +1,54 @@
-import React from "react";
-import { useRouter } from "next/router";
-import { Dropdown, DropdownButton } from "react-bootstrap";
+import React from 'react';
+import { useRouter } from 'next/router';
+import logo from '../../util/job-y-jobs.jpeg';
+import { Dropdown, DropdownButton, Image } from 'react-bootstrap';
 export default function VerticalNavBar({
-  sidebar,
-  toggleSidebar,
-  filter,
-  summary,
+	sidebar,
+	toggleSidebar,
+	filter,
+	summary,
 }) {
-  const router = useRouter();
-  const filterString = "Filtro: " + (filter == "home" ? "Nenhum" : filter);
+	const router = useRouter();
+	const filterString = 'Filtro: ' + (filter == 'home' ? 'Nenhum' : filter);
 
-  const filterRedirect = function (item) {
-    if (typeof window !== "undefined") {
-      router.push({
-        pathname: "/[filter]/[page]",
-        query: { filter: item, page: 1 },
-      });
-      toggleSidebar();
-    }
-  };
+	const filterRedirect = function (item) {
+		if (typeof window !== 'undefined') {
+			router.push({
+				pathname: '/[filter]/[page]',
+				query: { filter: item, page: 1 },
+			});
+			toggleSidebar();
+		}
+	};
 
-  return (
-    <div className={"vertical-nav " + (sidebar ? "active" : "")} id="sidebar">
-      <div className="py-4 px-3 mb-4 d-flex flex-column">
-        <DropdownButton
-          id="dropdown-basic-button"
-          title={filterString}
-          className="text-center w-100"
-        >
-          <Dropdown.Item onClick={() => filterRedirect("home")}>
-            Nenhum
-          </Dropdown.Item>
-          {Object.keys(summary).map((item) => (
-            <Dropdown.Item key={item} onClick={() => filterRedirect(item)}>
-              {item}
-            </Dropdown.Item>
-          ))}
-        </DropdownButton>
-      </div>
+	return (
+		<div className={'vertical-nav ' + (sidebar ? 'active' : '')} id='sidebar'>
+			<div className='py-4 px-3 mb-4 d-flex flex-column'>
+				<Image src={logo} />
+				<DropdownButton
+					id='dropdown-basic-button'
+					title={filterString}
+					className='text-center w-100'>
+					<Dropdown.Item onClick={() => filterRedirect('home')}>
+						Nenhum
+					</Dropdown.Item>
+					{Object.keys(summary).map((item) => (
+						<Dropdown.Item key={item} onClick={() => filterRedirect(item)}>
+							{item}
+						</Dropdown.Item>
+					))}
+				</DropdownButton>
+			</div>
 
-      <div
-        id="sidebarCollapse"
-        className={sidebar ? "rotate" : ""}
-        onClick={() => toggleSidebar()}
-        type="button"
-      >
-        <img src="/images/back.svg" />
-      </div>
+			<div
+				id='sidebarCollapse'
+				className={sidebar ? 'rotate' : ''}
+				onClick={() => toggleSidebar()}
+				type='button'>
+				<img src='/images/back.svg' />
+			</div>
 
-      {/* <div className="py-4 px-3 mb-4 profile">
+			{/* <div className="py-4 px-3 mb-4 profile">
         <div className="media-body text-center">
           <h4 className="mb-0 media-content">Rahul Yaduvanshi</h4>
           <p className=" mb-0 media-sub-content">UI/UX Developer</p>
@@ -88,6 +88,6 @@ export default function VerticalNavBar({
           </a>
         </li>
       </ul> */}
-    </div>
-  );
+		</div>
+	);
 }
